c913a1cac60a3e8641d4c8689e15fe21ab5bf947
yashh-billionbasecamp <yash.bhosale@billionbasecamp.com>
2025-11-25 19:35:49 +0530

    feat: updated file table added uploaded_by and uploaded_at

--- nucleus/models/tsm_models/files.py
+++ nucleus/models/tsm_models/files.py
@@ -2,6 +2,8 @@ from nucleus.db.database import Base
 from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy.types import DateTime
 import uuid
 
 class File(Base):
@@ -10,5 +12,8 @@ class File(Base):
     id:Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     file_name:Mapped[str] = mapped_column(String, nullable=False)
     file_path:Mapped[str] = mapped_column(String, nullable=False)
+    uploaded_by:Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
+    uploaded_at:Mapped[datetime] = mapped_column(DateTime(timezone=False), nullable=False)
+
 
 

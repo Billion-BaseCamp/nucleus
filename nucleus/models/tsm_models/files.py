d7402c64bce4b6f56a7680655b7aa06bb2af3d12
yashh-billionbasecamp <yash.bhosale@billionbasecamp.com>
2025-11-25 11:48:27 +0530

    feat: new files table

---
+++ nucleus/models/tsm_models/files.py
@@ -0,0 +1,14 @@
+from nucleus.db.database import Base
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+import uuid
+
+class File(Base):
+    __tablename__ = "files"
+
+    id:Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    file_name:Mapped[str] = mapped_column(String, nullable=False)
+    file_path:Mapped[str] = mapped_column(String, nullable=False)
+
+

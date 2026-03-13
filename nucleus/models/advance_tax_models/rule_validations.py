5ab1163fd85396b69b4b8a3bd1bcd6fff25f67be
yashh-billionbasecamp <yash.bhosale@billionbasecamp.com>
2026-03-13 14:49:35 +0530

    fix: updated the table

--- nucleus/models/advance_tax_models/rule_validations.py
+++ nucleus/models/advance_tax_models/rule_validations.py
@@ -1,24 +1,42 @@
 from nucleus.db.database import Base
-from sqlalchemy import UUID as SQLUUID, String
+from sqlalchemy import UUID as SQLUUID, String, ForeignKey, DateTime, Boolean
 from uuid import UUID, uuid4
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from datetime import datetime
-from sqlalchemy import ForeignKey, DateTime, Boolean
 from sqlalchemy.sql import func
+from datetime import datetime
 
 
 class RuleValidations(Base):
     __tablename__ = "rule_validations"
+
     id: Mapped[UUID] = mapped_column(SQLUUID(as_uuid=True), primary_key=True, default=uuid4)
-    rule_id:Mapped[str] = mapped_column(String, nullable=False)
-    rule_description:Mapped[str] = mapped_column(String, nullable=False)
-    quarter_id:Mapped[UUID] = mapped_column(SQLUUID(as_uuid=True), ForeignKey("quarters.id", ondelete="CASCADE"), nullable=False)
-    advisor_id:Mapped[UUID] = mapped_column(SQLUUID(as_uuid=True), nullable=False)
-    override_rule:Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
-    comments:Mapped[str] = mapped_column(String, nullable=False)
-    created_at:Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at:Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
 
-    quarter:Mapped["Quarter"] = relationship("Quarter", back_populates="rule_validations")
+    rule_id: Mapped[str] = mapped_column(String, nullable=False)
+    rule_description: Mapped[str] = mapped_column(String, nullable=False)
 
+    quarter_id: Mapped[UUID] = mapped_column(
+        SQLUUID(as_uuid=True),
+        ForeignKey("quarters.id", ondelete="CASCADE"),
+        nullable=False
+    )
 
+    advisor_id: Mapped[UUID] = mapped_column(SQLUUID(as_uuid=True), nullable=False)
+
+    override_rule: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    comments: Mapped[str] = mapped_column(String, nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+    )
+
+    quarter: Mapped["Quarter"] = relationship("Quarter", back_populates="rule_validations")

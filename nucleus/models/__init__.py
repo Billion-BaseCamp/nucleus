39313295fb8084ccd9e02d2bd3a529a48f8b0eb5
yashh-billionbasecamp <yash.bhosale@billionbasecamp.com>
2026-03-14 18:48:25 +0530

    fix:import carry forward table in init

--- nucleus/models/__init__.py
+++ nucleus/models/__init__.py
@@ -60,7 +60,7 @@ from nucleus.models.client_profiling.citizenship import Citizenship
 from nucleus.models.client_profiling.address import AddressType
 from nucleus.models.advance_tax_models.form_26as_models import Form26ASPart1, Form26ASPart2, Form26ASPart3, Form26ASPart4, Form26ASPart5, Form26ASPart6, Form26ASPart7, Form26ASPart8, Form26ASPart9, Form26ASPart10
 from nucleus.models.advance_tax_models.rule_validations import RuleValidations
-
+from nucleus.models.advance_tax_models.carry_forward_losses import CarryForwardLosses
 
 __all__ = [
     # Common Models
@@ -130,4 +130,5 @@ __all__ = [
     "Form26ASPart9",
     "Form26ASPart10",
     "RuleValidations",
+    "CarryForwardLosses",
 ]

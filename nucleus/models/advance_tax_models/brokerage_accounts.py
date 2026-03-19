53fee38e747fc26f8bf28cd6aa349c49d07f9992
yashh-billionbasecamp <yash.bhosale@billionbasecamp.com>
2026-03-19 14:26:06 +0530

    fix: added tds field in brokerage accounts

--- nucleus/models/advance_tax_models/brokerage_accounts.py
+++ nucleus/models/advance_tax_models/brokerage_accounts.py
@@ -14,6 +14,7 @@ class BrokerageAccounts(Base):
     capital_gains_id: Mapped[UUID] = mapped_column(SQLUUID(as_uuid=True), ForeignKey("capital_gains.id", ondelete="CASCADE"), nullable=False)
     account_name: Mapped[str] = mapped_column(String, nullable=True)
     account_value: Mapped[Decimal] = mapped_column(Numeric[Decimal](18,2), nullable=True)
+    tds_amount: Mapped[Decimal] = mapped_column(Numeric[Decimal](18,2), nullable=True)
     is_exempt: Mapped[bool] = mapped_column(Boolean, nullable=True)
     category: Mapped[str] = mapped_column(String, nullable=True)
     sub_category: Mapped[str] = mapped_column(String, nullable=True)

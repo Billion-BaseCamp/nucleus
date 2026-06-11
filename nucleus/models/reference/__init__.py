ef70714f99f788e354a64ac53904fd9d2d6fb181
Yash Bhosale <yash.bhosale@billionbasecamp.com>
2026-06-11 19:26:37 +0530

    feat: added amfi tables

---
+++ nucleus/models/reference/__init__.py
@@ -0,0 +1,8 @@
+"""Shared reference / master data models (not tied to a single ITR return)."""
+
+from nucleus.models.reference.amfi_mf import AmfiMasterMeta, AmfiMfIsin
+
+__all__ = [
+    "AmfiMasterMeta",
+    "AmfiMfIsin",
+]

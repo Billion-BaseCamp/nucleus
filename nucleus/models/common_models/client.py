7abf97033cb883793c6a920434aecf7edcf3ec6a
Samarth <samarth.bide@billionbasecamp.com>
2026-07-13 12:44:32 +0530

    feat : aconvert balance tax filed  string to decimal

--- nucleus/models/common_models/client.py
+++ nucleus/models/common_models/client.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 from datetime import date, datetime
+from decimal import Decimal
 from typing import List, Optional
 from uuid import UUID, uuid4
 
-from sqlalchemy import Boolean, Date, DateTime, Enum, ForeignKey, String, UniqueConstraint, UUID as SQLUUID
+from sqlalchemy import Boolean, Date, DateTime, Enum, ForeignKey, Numeric, String, UniqueConstraint, UUID as SQLUUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
@@ -40,7 +41,7 @@ class Client(Base):
     deactivated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
     it_portal_username: Mapped[str] = mapped_column(String, nullable=True)
     it_portal_pass: Mapped[str] = mapped_column(String, nullable=True)
-    balance_tax: Mapped[Optional[str]] = mapped_column(String(20), nullable=True)
+    balance_tax: Mapped[Optional[Decimal]] = mapped_column(Numeric(15, 2), nullable=True)
 
     
     # Foreign key to advisor
